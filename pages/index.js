8d8e7282eec2478ebdb753b03763e06dc50b8b9c
Victor hernandez <vhernandez.99@hotmail.com>
2022-04-30 14:04:41 -0500

    mp4 to webm

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,12 @@ export default function Home() {
         <div className="w-1/2 flex justify-between">
           <div className="flex items-center">
             <div>
-              <Image src="/assets/logo.webp" width={50} height={50} />
+              <Image
+                src="/assets/logo.png"
+                width={50}
+                height={50}
+                objectFit="cover"
+              />
             </div>
             <h1 className="tracking-wider font-bold text-mainColor-100">
               The Bit Museum
@@ -35,35 +40,42 @@ export default function Home() {
           <div className="relative w-1/2">
             {/* <Image src="/assets/poster.png" layout="fill"/> */}
             <video
-              src="/assets/ticket.mp4"
+              src="/assets/ticket.webm"
               autoPlay
               muted
               loop
               width={1500}
               height={1500}
+              type="video/mp4"
             ></video>
           </div>
-          <div className=" w-1/2 text-mainColor-100 pl-4 text-justify tracking-wide space-y-4">
-            <h1 className="text-center font-bold">Become a TBM Founder!</h1>
-            <p>
-              By owning a ticket, you will kickstart the project. Being a
-              Founder of the first Historic Metaverse Museum rewards you with
-              the following benefits.
-            </p>
-            <ul>
-              <li>
-                1. Free access to any future Museum opening in our metaverse
-                platform.
-              </li>
-              <li>2. Exclusive access to mints from Museum Partners</li>
-              <li>3. Reward over 10% of TBM utilities:</li>
-              <li>• All Sold Historical NFTs listed by the TBM</li>
-              <li>• TBM’s Metaverse Museum Ticket Services</li>
-              <li>
-                • Mesh-Texture Digitization Service (Paid like dividends every 3
-                months)
-              </li>
-            </ul>
+          <div className=" w-1/2 text-mainColor-100 pl-6 text-justify tracking-wide space-y-4">
+            <div>
+              <h1 className="text-center font-bold">Become a TBM Founder!</h1>
+              <p>
+                By owning a ticket, you will kickstart the project. Being a
+                Founder of the first Historic Metaverse Museum rewards you with
+                the following benefits.
+              </p>
+              <ul>
+                <li>
+                  1. Free access to any future Museum opening in our metaverse
+                  platform.
+                </li>
+                <li>2. Exclusive access to mints from Museum Partners</li>
+                <li>3. Reward over 10% of TBM utilities:</li>
+                <li>• All Sold Historical NFTs listed by the TBM</li>
+                <li>• TBM’s Metaverse Museum Ticket Services</li>
+                <li>
+                  • Mesh-Texture Digitization Service (Paid like dividends every
+                  3 months)
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h1>How many tickets to mint?</h1>
+              <input className="border border-mainColor-100 rounded-lg p-2" type="number" placeholder="0"/>
+            </div>
           </div>
         </div>
       </div>
